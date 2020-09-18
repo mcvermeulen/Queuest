4c7a04dffb729ffcf8671ebbce1642396991a0be
Marcel Vermeulen <vermeulen.mc@gmail.com>
2020-09-18 11:00:27 +0200

    init

---
+++ Coronatest.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+class Coronatest
+{
+    PriorityQueue<Aanvraag> testaanvragen = new PriorityQueue<Aanvraag>();
+    Random random = new Random();
+
+    public void Add(Aanvraag A) {
+        testaanvragen.Enqueue(BepaalPrioriteit(A), A);
+    }
+
+    public bool Empty() {
+        if (testaanvragen.Count == 0 ) return true;
+        return false;
+    }
+
+    public void UpdatePrioriteit (Aanvraag A, int nieuwePrioriteit) {
+        testaanvragen.UpdatePriority(A, nieuwePrioriteit);
+    }
+
+    public Uitslag Test() {
+        Aanvraag aanvraag = testaanvragen.Dequeue();
+        Uitslag uitslag = new Uitslag(aanvraag);
+
+        if (random.Next(0, 30) % 2 == 0) {
+            uitslag.Positief = true;
+        }
+
+        return uitslag;
+    }
+
+    private int BepaalPrioriteit(Aanvraag aanvraag) {
+        List<String> hogePrioriteitBeroepen = new List<String> {
+            "docent", "zorg"
+        };
+
+        if (hogePrioriteitBeroepen.Contains(aanvraag.Beroep.ToLower())) return 2;
+
+        return 1;
+    }
+}
+
+
+class Aanvraag
+{
+    public string Naam { get; set; }
+    public string Beroep { get; set; }
+    public DateTime AanvraagDatum { get; set; }
+
+    public Aanvraag(String naam, String beroep)
+    {
+        Naam = naam;
+        Beroep = beroep;
+        AanvraagDatum = DateTime.Now;
+    }
+}
+
+class Uitslag 
+{
+    public bool Positief { get; set; }
+    public Aanvraag Aanvraag { get; set; }
+
+    public Uitslag(Aanvraag aanvraag)
+    {
+        Aanvraag = aanvraag;
+        Positief = false;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Is {0} ({1}) positief? {2}", Aanvraag.Naam, Aanvraag.Beroep, Positief.ToString());
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Queuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coronatest testlocatie = new Coronatest();
+
+            Aanvraag aanvraag1 = new Aanvraag("Persoon1", "Zorg");
+            Aanvraag aanvraag2 = new Aanvraag("Persoon2", "Militair");
+            Aanvraag aanvraag3 = new Aanvraag("Persoon3", "Docent");
+            Aanvraag aanvraag4 = new Aanvraag("Persoon4", "Docent");
+            Aanvraag aanvraag5 = new Aanvraag("Persoon5", "iets");
+            Aanvraag aanvraag6 = new Aanvraag("Persoon6", "geen");
+            Aanvraag aanvraag7 = new Aanvraag("Persoon7", "Straatventer");
+            Aanvraag aanvraag8 = new Aanvraag("Persoon8", "Zorg");
+
+            testlocatie.Add(aanvraag1);
+            testlocatie.Add(aanvraag2);
+            testlocatie.Add(aanvraag3);
+            testlocatie.Add(aanvraag4);
+            testlocatie.Add(aanvraag5);
+            testlocatie.Add(aanvraag6);
+            testlocatie.Add(aanvraag7);
+            testlocatie.Add(aanvraag8);
+
+            testlocatie.UpdatePrioriteit(aanvraag7, 4);
+
+            do {
+                Uitslag uitslag = testlocatie.Test();
+
+                Console.WriteLine(uitslag.ToString());
+            } while (testlocatie.Empty() == false);
+                
+        }
+    }
+}
